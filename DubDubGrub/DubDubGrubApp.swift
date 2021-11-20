0ca27bdefda0ef010b6412b77859eb9ee609d871
Amir Eghtedari <eghtedaria.amir@gmail.com>
2021-11-20 22:39:13 +0400

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Amirreza Eghtedari on 20.11.21.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
