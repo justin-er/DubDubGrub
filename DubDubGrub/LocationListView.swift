40d2229cea2229101c393461af7c35b9f2615a9d
Amir Eghtedari <eghtedaria.amir@gmail.com>
2022-10-16 19:04:45 +0200

    Add TabView

---
+++ DubDubGrub/LocationListView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationListView.swift
+//  DubDubGrub
+//
+//  Created by Justin on 16.10.22.
+//
+
+import SwiftUI
+
+struct LocationListView: View {
+    var body: some View {
+        Text("Location List")
+    }
+}
+
+struct LocationListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationListView()
+    }
+}
---
+++ DubDubGrub/LocationMapView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationMapView.swift
+//  DubDubGrub
+//
+//  Created by Justin on 16.10.22.
+//
+
+import SwiftUI
+
+struct LocationMapView: View {
+    var body: some View {
+        Text("Map View")
+    }
+}
+
+struct LocationMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationMapView()
+    }
+}
